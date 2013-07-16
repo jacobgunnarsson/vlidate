10065fa04bdb1c688712087fe67bcc72111fef0c
Jacob Gunnarsson <jgunnarsson@domanistudios.com>
2013-07-16 16:13:06 -0400

    Woot, functional, version 1.0

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,7 +9,12 @@ module.exports = function(grunt) {
                 mangle: true,
                 compress: true,
                 report: 'gzip',
-                banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %> */\n\n'
+                banner: '/*!\n<%= pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n\
+                        \nAuthor: Jacob Gunnarsson\
+                        \nContact: hello@jacobgunnarsson.com\
+                        \n\nhttp://www.jacobgunnarsson.com/\
+                        \nhttps://github.com/jacobgunnarsson\
+                        \n*/\n\n'
             },
             dist: {
                 files: {
--- src/jquery.vlidate.js
+++ src/jquery.vlidate.js
@@ -1,25 +1,34 @@
 ;(function($, window, document, undefined) {
 
 	$.fn.vlidate = function(options, callback) {
-		var ENV = 'dev',
-            form = this;
+		var form = this;
 
         /*
         *   The default settings and validation regex for supported inputs
         */
 		var defaults = {
 
+            preventFormSubmit: true,
+
+            highlight: false,
+
+            highlightClasses: {
+                failed: 'vlidate-failed',
+                passed: 'vlidate-passed'
+            },
+
             regex: {
                 name: /^[a-zA-Z ]+$/,
                 email: /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-                password: /(?=^.{6,}$)((?=.*[A-Za-z0-9])(?=.*[A-Z])(?=.*[a-z]))^.*/,
+                password: /^[a-zA-Z0-9 ]+$/,
                 cc: /^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|6011[0-9]{12}|3(?:0[0-5]|[68][0-9])[0-9]{11}|3[47][0-9]{13})$/,
                 url: /^(((http|https|ftp):\/\/)?([[a-zA-Z0-9]\-\.])+(\.)([[a-zA-Z0-9]]){2,4}([[a-zA-Z0-9]\/+=%&_\.~?\-]*))*$/,
                 alpha: /^[a-zA-Z]+$/,
                 alphalower: /^([a-z])*$/,
                 alphaupper: /^([A-Z])*$/,
                 alphanum: /^[a-zA-Z0-9]+$/,
-                numeric: /^[0-9]+$/
+                numeric: /^[0-9]+$/,
+                text: /^[a-zA-Z0-9 ]+$/
             }
 
         };
@@ -36,7 +45,7 @@
 
         var regex = defaults.regex;
 
-        var results;
+        var results = {};
 
 		/*
 		*	Prevent native HTML5 form validation and bind event handler on form submit
@@ -44,29 +53,43 @@
         form.attr('novalidate', 'novalidate');
 
 		form.on('submit', function(event) {
-            event.preventDefault();
+
+            /*
+            *   See if user has overriden option to prevent form submission (to handle form submission in callback), if so, submit form as usual
+            */
+            if (typeof results.status !== 'undefined')
+                if (!defaults.preventFormSubmit && results.status === 'passed') return true;
+
+            if (defaults.preventFormSubmit || results.status !== 'passed') event.preventDefault();
 
             /*
             *   Results to store validation results
             */
-            results = { failed: [], success: [] };
+            results = { status: '', failed: [], passed: [] };
 
             /*
             *   Iterate over inputs with validate data-attribute and use validation method based on tagName
             */
 			inputs.each(function(v, el) {
+                var $this       = $(el),
+                    inputVal    = $this.val(),
+                    isCheckbox  = ($this.attr('type') === 'checkbox'),
+                    isSelect    = ($this.prop('tagName') === 'SELECT'),
+                    validation  = $this.data('validate');
 
-                if ($(el).data('validate')) {
+                if (validation) {
+
+                    if (typeof regex[validation] === 'undefined' && !isCheckbox && !isSelect) methods.error('Invalid data-validate attribute');
 
                     switch (el.tagName) {
                         case 'INPUT':
-                            methods.validate.input(el);
+                            methods.validate.input($this, inputVal, isCheckbox, validation);
                             break;
                         case 'SELECT':
-                            methods.validate.select(el);
+                            methods.validate.select($this, inputVal, validation);
                             break;
                         case 'TEXTAREA':
-                            methods.validate.textarea(el);
+                            methods.validate.textarea($this, inputVal, validation);
                             break;
                     }
 
@@ -75,8 +98,10 @@
 			});
 
             /*
-            *   Handle results, submit form and handle callback
+            *   Highlight results, submit form and call callback
             */
+            if (defaults.highlight) methods.highlight();
+
             methods.submit();
 
 		});
@@ -88,11 +113,7 @@
             */
             validate: {
 
-                input: function(el) {
-                    var $this       = $(el),
-                        inputVal    = $this.val(),
-                        isCheckbox  = !!($this.attr('type') === 'checkbox'),
-                        validation  = $this.data('validate');
+                input: function($this, inputVal, isCheckbox, validation) {
 
                     /*
                     *   Test for empty value
@@ -115,7 +136,7 @@
                     /*
                     *   Test text input against regex
                     */
-                    if (!isCheckbox && !inputVal.test(regex[validation])) {
+                    if (!isCheckbox && !regex[validation].test(inputVal)) {
                         results.failed.push($this);
 
                         return;
@@ -124,17 +145,57 @@
                     /*
                     *   Store form elements that passed validation
                     */
-                    results.success.push($this);
+                    results.passed.push($this);
 
                 },
 
-                select: function(el) {
+                select: function($this, inputVal, validation) {
+                    var defaultValue = $('option', $this).eq(0);
+
+                    /*
+                    *   Test for a required select
+                    */
+                    if (inputVal === '' || inputVal === defaultValue) {
+                        results.failed.push($this);
+
+                        return;
+                    }
+
+                    results.passed.push($this);
 
                 },
 
-                textarea: function(el) {
+                textarea: function($this, inputVal, validation) {
 
+                    /*
+                    *   Test for a required select
+                    */
+                    if (inputVal === '') {
+                        results.failed.push($this);
+
+                        return;
+                    }
+
+                    results.passed.push($this);
                 }
+
+            },
+
+            highlight: function() {
+                var failedClass = defaults.highlightClasses.failed,
+                    passedClass = defaults.highlightClasses.passed;
+
+                /*
+                *   Hightlight elements that failed validation
+                */
+                for (var i = 0; i < results.failed.length; ++i)
+                    results.failed[i].removeClass(passedClass).addClass(failedClass);
+
+                /*
+                *   Highlight elements that passed validation
+                */
+                for (var i = 0; i < results.passed.length; ++i)
+                    results.passed[i].removeClass(failedClass).addClass(passedClass);
 
             },
 
@@ -143,7 +204,13 @@
             */
             submit: function() {
 
-                if (typeof callback === 'function') {
+                results.status = (results.failed.length) ? 'failed' : 'passed';
+
+                if (!defaults.preventFormSubmit && results.status === 'passed') {
+
+                    form.submit();
+
+                } else if (typeof callback === 'function' ) {
 
                     callback(results);
 
